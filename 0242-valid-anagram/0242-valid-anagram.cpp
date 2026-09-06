b6209df511c3539b322710d3491ed02bfcbd6c2f
palaksingh35 <palaksingh35012@gmail.com>
2026-09-06 14:17:10 +0530

    Time: 3 ms (45.27%), Space: 9.6 MB (92.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+         if(s.size() != t.size()) {
+            return false;
+        }
+        int count[26] = {0};
+        for(int i=0;i<s.size();i++){
+         count[s[i] - 'a']++;
+        }
+        for(int j=0;j<t.size();j++){
+             count[t[j] - 'a']--;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(count[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
