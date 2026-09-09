2f31522ffb3bcb1e0f7fa6d405366e18f5f7b329
palaksingh35 <palaksingh35012@gmail.com>
2026-09-09 19:48:51 +0530

    Time: 3 ms (58.34%), Space: 121.9 MB (42.56%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -9,28 +9,46 @@
  * };
  */
 class Solution {
-    bool checkPalindrome(vector<int> arr){
-        int n= arr.size();
-        int s=0;
-        int e= n-1;
-        while(s <= e){
-            if(arr[s]!= arr[e]){
-                return false;
-            }
-            s++;
-            e--;
+    ListNode* getMid(ListNode* head){
+     ListNode* slow=head;
+     ListNode* fast= head->next;
+     while(fast!= NULL && fast->next!= NULL){
+        fast= fast->next->next;
+        slow=slow->next;
+     }
+     return slow;
+    }
+    ListNode* reverse(ListNode* head){
+        ListNode* curr=head;
+        ListNode* prev=NULL;
+        ListNode* next=NULL;
+        while(curr!= NULL){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+
         }
-        return 1;
+        return prev;
     }
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp= head;
-        while(temp!= NULL){
-            arr.push_back(temp->val);
-            temp= temp->next;
+        if(head->next==NULL){
+            return true;
         }
-        return checkPalindrome(arr);
+        ListNode* mid=getMid(head);
+        ListNode* temp=mid->next;
+        mid->next=reverse(temp);
+        ListNode* head1 = head;
+        ListNode* head2 = mid->next;
+        while(head2!= NULL){
+         if(head2->val != head1->val) {
+                return 0;
+            }
+            head1 = head1 ->next;
+            head2 = head2 ->next;
+        }
         
+         return true;
     }
 };
