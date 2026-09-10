bc6f938530d514fae97d0abc5afda48b193acdea
palaksingh35 <palaksingh35012@gmail.com>
2026-09-10 19:40:38 +0530

    Time: 0 ms (100%), Space: 9.9 MB (8.6%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+     
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(s[i] == '(')
+                st.push(')');
+
+            else if(s[i] == '[')
+                st.push(']');
+
+            else if(s[i] == '{')
+                st.push('}');
+
+            else {
+                if(st.empty() || st.top() != s[i])
+                    return false;
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
+    
